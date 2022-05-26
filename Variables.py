f6b8b92bc0c2ac8e22e4aec2d4e672c483fd01be
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-05-26 12:13:01 -0500

    Variable Basics
    Variable Basics

---
+++ Variables.py
@@ -0,0 +1,46 @@
+""" Nigele McCoy
+Variable examples
+"""
+#"""
+# Data
+# An integer
+age = 17
+  
+# A floating point
+salary = 3556.8
+  
+# A string
+name = "John"
+
+# print variables
+print(age)
+print(salary)
+print(name)
+
+# Context : meaning
+print(f'{name} is {age} and makes {salary} a year')
+#"""
+
+"""
+# Boolean 
+lightStatus = True
+print(f"The light is on if the value is {lightStatus}")
+lightStatus = False
+print(f"The light is on if the value is {lightStatus}")
+"""
+
+"""
+firstName = 'Mary'
+lastName = 'Jane'
+full_name = f'{firstName} {lastName}'
+print(full_name)
+full_name = "Kodak" + " " + "Black"
+print(full_name)
+"""
+
+"""
+a = 36
+b = 64
+c= a + b
+print(a+b,'=',c)
+"""
