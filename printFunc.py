86c4b76807c07c140d9c8419c3e1b11bc527ab2d
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-05-26 11:25:05 -0500

    Add files via upload

---
+++ printFunc.py
@@ -0,0 +1,22 @@
+# Nigele McCoy
+# print program
+"""
+This is the first program we will run
+Program shows how the print function works
+
+"""
+
+print("Hello World")
+
+#print()
+#print(1+2)
+#print(1*9*3/2)
+#print('hello class')
+#print("hello class")
+#print("Hello class","my name is Nigele", "I will be your instructor")
+#print("Hello class","my name is Nigele", "I will be your instructor",sep='\n')
+#print("Hello class","my name is Nigele", "I will be your instructor",end=".")
+
+# directory dir() and Help help() function
+#dir()
+#help()
