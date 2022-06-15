9b52c8f9ddcc5e67ec37fdd052b25098abb2446d
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-06-15 14:54:36 -0500

    if else statements
    if else statements

---
+++ if_else.py
@@ -0,0 +1,23 @@
+# if else statements
+a = 100
+if a > 100:
+  print(a,"greater than 100")
+else:
+  print(a,"is not greater than 100")
+b = 50
+if b >= 50:
+  print(b,"greater than or equal to 50")
+else:
+  print(b,"is not greater than 50")
+
+c = 50
+if c <= 50:
+  print(c,"less than or equal to 50")
+else:
+  print(c,"is less than 50")
+
+if c != 60:
+  print(c, "is not equal to 60")
+else:
+  print(c, "is equal to 60")
+
