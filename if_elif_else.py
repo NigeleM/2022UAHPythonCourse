dc86e5d154d48320ae0d1534bea28fc308cf51d0
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-06-20 13:31:49 -0500

    if elif else statements
    if elif else statements

---
+++ if_elif_else.py
@@ -0,0 +1,12 @@
+
+# Python conditional statement
+# if you want more than if else
+# you can use elif to add more than two conditions
+a = 100
+
+if a > 100:
+  print(a, 'is greater than 100')
+elif a < 100:
+  print(a,'is less than 100')
+else:
+  print(a,'is equal to 100')
