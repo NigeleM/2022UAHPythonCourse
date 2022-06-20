6d9d5eb11d373e51c088bd85e0404dc81962f967
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-06-20 13:36:59 -0500

    and_or_not
    and_or_not

---
+++ and_or_not.py
@@ -0,0 +1,37 @@
+# boolean logic 
+# and multiple conditions
+# we use AND OR to create multiple conditions within an if statement. 
+
+# multiple conditions
+if True:
+  print('Hello Class')
+else:
+  print('Goodbye Class')
+
+if not True:
+  print('Hello Class')
+else:
+  print('Goodbye Class')
+
+if False:
+  print('Hello Class')
+else:
+  print('Goodbye Class')
+
+if not False:
+  print('Hello Class')
+else:
+  print('Goodbye Class')
+
+a = 100
+
+if a > 98 or a == 100:
+  print('A is equal to 100 or it is greater than 99')
+else:
+  print('A is not equal to 100 or it is less than 99')
+
+if a > 99 and a == 100:
+  print('A is equal to 100 or it is greater than 99')
+else:
+  print('A is not equal to 100 or it is less than 99')
+  
