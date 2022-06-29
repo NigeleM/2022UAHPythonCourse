f78e119c70e8b4dd1dadc5885d53c484ca83f47e
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-06-29 11:04:05 -0500

    For_Loop
    For_Loop

---
+++ For_Loop.py
@@ -0,0 +1,38 @@
+for letter in 'vivi':
+    print(letter)
+print()
+
+for i in range(0,10):
+    print(i)
+
+
+# range(start,stop,skip)
+# for i in range(0,11,2):
+#      print(i)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#    continue
+#   print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#      break
+#   print(x)
+
+# for x in range(6):
+#    if x == 4:
+#      break
+# else:
+#   print("Finally finished!")
+# #If the loop breaks, the else block is not executed.""" 
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "tomato", "cherry"] 
+
+# for x in adj:
+#    for y in fruits:  
+#     if x == "big" and y == "tomato":
+#       break 
+#     print(x, y)
