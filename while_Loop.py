b14afa902f994b80807410bad688013ecc0478c0
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-06-29 11:05:13 -0500

    while_Loop
    while_Loop

---
+++ while_Loop.py
@@ -0,0 +1,45 @@
+i = 1
+while i < 6:
+  print(i)
+  i = i + 1
+  #i += 1
+
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 3:
+#     break
+#   i = i + 1
+
+
+# i = 0
+# while i < 6:
+#   i += 1 
+#   if i == 3:
+#     continue
+#   print(i)
+
+# i = 0
+# a = True
+# while a:
+#     if i == 5:
+#         print(i,'== 5')
+#     elif i == 8:
+#         a = False
+#         #print(i)
+#     print(i)
+#     i += 1
+
+# what not to do
+# infinite loops
+# a = 15400
+# while a > -1:
+#   print(a)
+#   a %= 3
+#    #a = a + 3
+
+# a = 1
+# while a > -1:
+#   print(a)
+#   a = a + 1
