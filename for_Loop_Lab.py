a9b1470ccd2b918ed6bbafb0d766ca641ecdd856
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-07-03 12:18:27 -0500

    for Loop Lab
    for Loop Lab

---
+++ for_Loop_Lab.py
@@ -0,0 +1,24 @@
+# get the total of all even numbers up to 100 including 100
+# for num in
+# total = 0
+# for num in range(0,101,2):
+#   total = num + total
+#   print(num,total)
+# print(total)
+  
+
+
+# get the total of all odd numbers up to 100
+# total = 0 
+# for num in range(1,100,2): 
+#   total = num + total 
+#   print(num, total)
+# print(total)
+
+
+
+ #create a list of names and loop through them  
+names = ['Nigele', 'Vivi','Nancy','you','Quan']
+for name in names:
+  print(name)
+  
