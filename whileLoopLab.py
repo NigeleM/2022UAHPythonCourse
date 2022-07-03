cbe95913d9a7d946ee338afc642478609ef18ec5
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-07-03 12:19:30 -0500

    while Loop Lab
    while Loop Lab

---
+++ whileLoopLab.py
@@ -0,0 +1,12 @@
+
+# create a while loop that gets the power of numbers 
+counter = 0
+while counter < 101:
+  print(counter,'\t\t',counter*counter)
+  #counter += 1
+  counter = counter + 1
+# of numbers up to 100 and prints them
+# output 1    
+# output 2    4
+
+
