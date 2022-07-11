fd7274d2d635d100290848398529df08b879b674
Nigele <86542859+NigeleM@users.noreply.github.com>
2022-07-11 13:41:39 -0500

    Homework Lab
    Homework Lab

---
+++ Homework_Lab.py
@@ -0,0 +1,69 @@
+# 1. 
+# enter a name , number , boolean variable
+# hint 
+myname = input("Enter a name : ")
+mynum = int(input("Enter a number : "))
+boolean = True
+contentList = []
+contentList.append(myname)
+contentList.append(mynum)
+contentList.append(boolean)
+print(contentList)
+
+
+# 2 loops 
+# Get max, min, and average of list
+# numlist = []
+# numCount = int(input("Enter how many number you would like to Enter : "))
+
+# for num in range(0,numCount):
+#   number = int(input("Enter number : "))
+#   numlist.append(number)
+
+# print("MAX",max(numlist))
+# print("MIN",min(numlist))
+# print("AVG",sum(numlist)/len(numlist))
+
+# 3 if else statements 
+# num  = int(input("Enter a number"))
+# if (num < 100 and num > 50):
+#   print('number is less than 100 but greater than 50')
+# else:
+#   print('number is less than 50 or greater than 100')
+
+# 4 Calculator Lab edit
+# put this in a while loop
+# hint see while_Loop_Examples on Github
+# status = True
+# while status == True:
+#   num1 = float(input("Enter a number : "))
+#   num2 = float(input("Enter a number 2 : "))
+#   operand = input("enter operator + / - * ** % :")
+#   if operand == "+":
+#     total = num1 + num2 
+#     print(total)
+#   elif operand == "-":
+#     total = num1 - num2 
+#     print(total)
+#   elif operand == "/" and num2 == 0:
+#     print("Cant divide by zero")
+#   elif operand == "/":
+#       total = num1 / num2 
+#       print(total)
+#   elif operand == "*":
+#     total = num1 * num2 
+#     print(total)
+#   elif operand == "**":
+#     total = num1 * num2 
+#     print(total)
+#   elif operand == "%":
+#     total = num1 % num2 
+#     print(total)
+
+# 5 fix these lines of code
+# print('hello world')
+# a = 1
+# print(a+1)
+# print([1, 2, 3, 4, 5])
+
+
